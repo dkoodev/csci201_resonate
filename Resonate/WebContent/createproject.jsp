589e00a8cdc2ebb2b4d7b4075ea9d103e0651a9c
isaac <isaac@Dodgeball>
2017-11-12 21:39:26 -0800

    CreateProject stuff.

--- Resonate/WebContent/createproject.jsp
+++ Resonate/WebContent/createproject.jsp
@@ -1,11 +1,13 @@
 <%@ include file="includes/global_header.jsp" %>
 <script type="text/javascript">
+var numFiles = 0;
 function addFileBtn() {
-	$('#filesHolder').append("<input type=\"file\" value=\"New File\" />");
+	$('#filesHolder').append("<input type=\"file\" name=\"file_"+ numFiles + "\" value=\"New File\" />");
+	numFiles++;
 }
 </script>
 <p>Change the world, one note at a time. It all starts with a good project.</p>
-<form action="CreateProject" method="POST">
+<form action="CreateProject" method="POST" enctype="multipart/form-data">
 	<input type="text" name="name" placeholder="Name" /><br />
 	<input type="text" name="description" placeholder="A short description" />
 	<p>Add some resources for others to build from:</p>
---
+++ Resonate/src/com/resonate/CreateProject.java
@@ -0,0 +1,94 @@
+package com.resonate;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+
+import com.resonate.objects.*;
+
+/**
+ * Servlet implementation class CreateProject
+ */
+@WebServlet("/CreateProject")
+public class CreateProject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateProject() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String proj_name = request.getParameter("name");
+    	String proj_desc = request.getParameter("description");
+    	
+    	System.out.println(proj_name);
+    	System.out.println(proj_desc);
+        HttpSession session = request.getSession();
+        
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        
+        System.out.println(isMultipart);
+
+        if (isMultipart) {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            try {
+                List items = upload.parseRequest(new ServletRequestContext(request));
+                Iterator iterator = items.iterator();
+                while (iterator.hasNext()) {
+                    FileItem item = (FileItem) iterator.next();
+
+                    if (!item.isFormField()) {
+                        String fileName = item.getName();
+                        
+                        System.out.println(fileName);
+                        
+                        String root = "/Resonate/uploads";
+                        File path = new File(root + "/projectResources");
+                        if (!path.exists()) {
+                            boolean status = path.mkdirs();
+                        }
+
+                        File uploadedFile = new File(path + "/" + fileName);
+                        System.out.println(uploadedFile.getAbsolutePath());
+                        item.write(uploadedFile);
+                    }
+                }
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        
+        
+
+        Project newProject = null;
+       
+       // if (JDBCDriver.createProject(proj_name, proj_desc, ))
+    }
+}
