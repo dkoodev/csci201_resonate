4aa85f60a91bbec8aa81de190305e025fa1c2cdd
Isaac Taylor <isaactay@usc.edu>
2017-11-20 20:03:28 -0800

    Loader.

--- Resonate/src/com/resonate/Downloading.java
+++ Resonate/src/com/resonate/Downloading.java
@@ -2,10 +2,12 @@ package com.resonate;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpCookie;
 import java.nio.file.Files;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +35,7 @@ public class Downloading extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	HttpSession session = request.getSession();
+    	response.addCookie(new Cookie("fileDownloadToken", "true"));
 		File file = (File) session.getAttribute("fileToDownload");
 		String fileName = (String) session.getAttribute("fileToDownloadFileName");
         response.setHeader("Content-Type", getServletContext().getMimeType(fileName));
